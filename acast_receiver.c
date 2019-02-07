a9df8db5e8e9c642dd87908364a49e9925f1e580
Tony Rogvall <tony@rogvall.se>
2019-02-07 18:51:20 +0100

    option to set output channels

--- acast_receiver.c
+++ acast_receiver.c
@@ -95,7 +95,7 @@ int main(int argc, char** argv)
 	    {0,        0,                 0, 0}
 	};
 	
-	c = getopt_long(argc, argv, "lhva:i:p:t:d:",
+	c = getopt_long(argc, argv, "lhva:i:p:t:d:c:",
                         long_options, &option_index);
 	if (c == -1)
 	    break;
